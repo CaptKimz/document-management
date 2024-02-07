bf6cce20f2c4562dde1686d6691de0f7add83edf
CaptKimz <85158497+CaptKimz@users.noreply.github.com>
2024-02-07 16:42:21 +0700

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, lazy, Suspense } from 'react';
+
 import {
   UploadOutlined, UserOutlined, VideoCameraOutlined, MenuFoldOutlined,
-  MenuUnfoldOutlined,
+  MenuUnfoldOutlined, CheckOutlined, CloseOutlined
 } from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
+import { Button, Layout, Menu, Switch, theme} from 'antd';
 import './App.css'
 const { Header, Content, Sider } = Layout;
 const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
@@ -13,61 +14,74 @@ const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].
     label: `nav ${index + 1}`,
   }),
 );
-
+const Home = lazy(() => import('./pages/HomePage'));
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App: React.FC = () => {
   const {
-    token: { colorBgContainer, borderRadiusLG },
+    token: { colorBgContainer, borderRadiusLG }
   } = theme.useToken();
   const [collapsed, setCollapsed] = useState(false);
   const [hideButton, setHideButton] = useState(false);
-  console.log("log", hideButton);
-
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Sider
-        // trigger={null}
-        collapsed={collapsed}
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-          setHideButton(!broken)
-        }}
-        onCollapse={() => {
-          // console.log(collapsed, type);
-          setCollapsed((data) => !data)
-        }}
-      >
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer, display: "flex" }} >
-          {hideButton ? <Button
-            type="text"
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-            onClick={() => setCollapsed(!collapsed)}
-            style={{
-              fontSize: '16px',
-              width: 64,
-              height: 64,
-            }}
-          /> : []}
-        </Header>
-        <Content style={{ margin: '24px 16px 0' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            content
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
+    <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Layout style={{ height: '100vh' }}>
+            <Sider
+              collapsed={collapsed}
+              breakpoint="lg"
+              collapsedWidth="0"
+              onBreakpoint={(broken) => {
+                setHideButton(!broken)
+              }}
+              onCollapse={() => {
+                setCollapsed((data) => !data)
+              }}
+            >
+              <div className="demo-logo-vertical" />
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+            </Sider>
+            <Layout>
+              <Header style={{ padding: 0,  display: "flex", alignItems: "center" }} >
+
+                {hideButton ? <Button
+                  type="text"
+                  icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                  onClick={() => setCollapsed(!collapsed)}
+                  style={{
+                    fontSize: '16px',
+                    width: 64,
+                    height: 64,
+                  }}
+                /> : []}
+                <Switch
+                onChange={(data)=>{
+                  console.log(data);
+                  setThemeMode("light")                 
+                }}
+                  checkedChildren={<CheckOutlined />}
+                  unCheckedChildren={<CloseOutlined />}
+                  defaultChecked
+                />
+              </Header>
+              <Content style={{ margin: '24px 16px 0' }}>
+                <div
+                  style={{
+                    padding: 24,
+                    minHeight: 360,
+                    background: colorBgContainer,
+                    borderRadius: borderRadiusLG,
+                  }}
+                >
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                  </Routes>
+                </div>
+              </Content>
+            </Layout>
+          </Layout>
+        </Suspense>
+    </BrowserRouter>
+
   );
 };
 
